88c82c32b6df57ba7d85b8787b2f68a8164ba22d
amrinder-pal-singh <amrinderpal.singh@outlook.com>
2017-04-08 21:26:18 +0530

    First Commit

---
+++ SATURAN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATURAN.Startup))]
+namespace SATURAN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
